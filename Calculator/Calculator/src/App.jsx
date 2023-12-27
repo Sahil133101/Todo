30eeb8c390de2fa07f0bd4b60239905252167998
Sahil Sharma <103818219+Sahil133101@users.noreply.github.com>
2023-12-27 21:16:12 +0530

    calculator

--- Calculator/Calculator/src/App.jsx
+++ Calculator/Calculator/src/App.jsx
@@ -3,24 +3,46 @@ import Display from "./Display";
 import Buton from "./Buton";
 import { useState } from "react";
 export default function App(){
+
+  const[result, setresult] = useState("");
+
+  const clickHandler = (value) => {
+    if(value === "="){
+      try{
+        setresult(eval(result).toString());
+
+      } catch(error){
+        setresult("Error");
+      }
+    }
+    else if (value === "clear"){
+      setresult("");
+    }
+    else if(value === "->"){
+      setresult(result.slice(0,-1))
+    }
+    else {
+      setresult(result.concat(value));
+    }
+  };
   
 
  
 
 
 
-  const Button = ["C", "DEL", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", " 3", " +","0", "." ,"="];
+  const Button = ["C", "DEL", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", " 3", " +","0", "." ,"=", "->"];
 
   return(
     <>
 
     <div  className= "Calculator">
-     <Display > </Display>
+     <Display  result = {result}> </Display>
        
 
         
         
-        <Buton item = {Button}></Buton>
+        <Buton  item = {Button} clickHandler={clickHandler}></Buton>
         
 
         
--- Calculator/Calculator/src/Buton.jsx
+++ Calculator/Calculator/src/Buton.jsx
@@ -1,12 +1,12 @@
 import Buttton from "./Buttton"
-export default function Buton ({item } ){
+export default function Buton ({item , clickHandler} ){
     return (
         
              <div className="ButtonContainer"> 
 
              {
              item.map((xyz) =>   (
-             <Buttton key  = {xyz} ButTon = {xyz}>  </Buttton> ))  }  
+             <Buttton key  = {xyz} onClick = {()=>clickHandler(xyz)} ButTon = {xyz}>  </Buttton> ))  }  
 
              </div>
 
